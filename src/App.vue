44a8c2c63a760fdb8a76bd718924945af04384db
陳国偉 <guowei.chen@hh-c.co.jp>
2021-08-27 14:31:48 +0900

    メイン固定に変更

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="max-w-full max-h-full min-w-min bg-backgroundMain">
-        <div class="max-w-full md:mr-25 md:ml-25 bg-white">
+    <div class="flex">
+        <div class="flex-grow max-h-full min-w-min bg-backgroundMain"></div>
+        <div
+            class="
+                flex-none
+                md:block
+                md:overflow-x-auto
+                w-full
+                md:w-270
+                bg-white
+            "
+        >
             <router-view />
         </div>
+        <div class="flex-grow max-h-full min-w-min bg-backgroundMain"></div>
     </div>
 </template>
