af4359f849fca3665951be1d4bc66e313ebac099
zhiwenjia08 <zhiwenjia5@gmail.com>
2021-08-27 15:05:37 +0900

    toTopICon update

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
         <div class="flex-grow max-h-full min-w-min bg-backgroundMain"></div>
         <div
             class="
+                relative
                 flex-none
                 md:block
                 md:overflow-x-auto
@@ -12,7 +13,42 @@
             "
         >
             <router-view />
+            <button
+                class="
+                    absolute
+                    h-15
+                    w-15
+                    bottom-5
+                    right-1.25
+                    bg-toTop
+                    text-white
+                    rounded-full
+                    md:right-12.5
+                    md:bottom-7.5
+                "
+                @click="toTop"
+            >
+                ▲
+            </button>
         </div>
         <div class="flex-grow max-h-full min-w-min bg-backgroundMain"></div>
     </div>
 </template>
+<script>export default {
+  methods: {
+    toTop() {
+      let currentScroll = document.documentElement.scrollTop,
+        int = setInterval(frame, 6)
+
+      function frame() {
+        if (0 > currentScroll) {
+
+          clearInterval(int)
+        } else {
+          currentScroll = currentScroll - 12
+          document.documentElement.scrollTop = currentScroll
+        }
+      }
+    }
+  },
+}</script>
--- src/layout/homelayout.vue
+++ src/layout/homelayout.vue
@@ -47,23 +47,6 @@
             <data-management-area></data-management-area>
         </div>
     </div>
-
-    <button
-        class="
-            fixed
-            h-15
-            w-15
-            bottom-5
-            right-1.25
-            bg-toTop
-            text-white
-            rounded-full
-            md:right-37.5
-        "
-        @click="toTop"
-    >
-        ▲
-    </button>
 </template>
 
 <script>
@@ -85,20 +68,6 @@ export default {
   couputed: {},
   watch: {},
   methods: {
-    toTop() {
-      let currentScroll = document.documentElement.scrollTop,
-        int = setInterval(frame, 6)
-
-      function frame() {
-        if (0 > currentScroll) {
-
-          clearInterval(int)
-        } else {
-          currentScroll = currentScroll - 12
-          document.documentElement.scrollTop = currentScroll
-        }
-      }
-    }
   },
   created() {
 
