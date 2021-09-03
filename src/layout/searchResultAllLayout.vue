73f0e2867ed4cc9dba5bdfd0321df10d429ad055
陳国偉 <guowei.chen@hh-c.co.jp>
2021-09-03 10:54:33 +0900

    一括検索結果

--- src/layout/searchResultAllLayout.vue
+++ src/layout/searchResultAllLayout.vue
@@ -1,9 +1,12 @@
 <template>
+    <!-- 検索枠 -->
     <div class="flex flex-row">
+        <!-- pcの場合 -->
         <div class="flex-auto h-15 md:h-20 hidden md:block">
             <search-bar></search-bar>
         </div>
 
+        <!-- SPの場合 -->
         <div
             class="flex-auto h-15 md:h-20 block md:hidden"
             :class="[
@@ -14,24 +17,20 @@
         >
             <search-bar></search-bar>
         </div>
+        <!-- spの場合、ヘッダー,、検索枠の位置を替える -->
         <div
             class="block md:hidden"
             :class="[$store.getters.getIsMenuClick ? '' : 'h-27.5']"
         ></div>
     </div>
+
     <div class="flex">
         <!-- 検索結果両側の左余白 -->
         <div class="flex-grow max-h-full min-w-min block"></div>
 
-        <div class="lex-shrink w-full md:w-191.25 space-y-3.75">
-            <div class="flex-grow space-y-3.75">
-                <div class="flex-col">
-                    <div class="grid grid-cols-1 gap-1 md:space-y-3.75">
-                        <search-result-main
-                            class="flex-grow"
-                        ></search-result-main>
-                    </div>
-                </div>
+        <div class="flex-shrink mr-2.5 ml-2.5 w-full md:w-191.25">
+            <div class="grid grid-cols-1 gap-1 md:space-y-3.75">
+                <search-result-main class="flex-grow"></search-result-main>
             </div>
         </div>
 
@@ -55,20 +54,7 @@ export default {
     };
   },
   methods: {
-    toTop() {
-      let currentScroll = document.documentElement.scrollTop,
-        int = setInterval(frame, 6)
 
-      function frame() {
-        if (0 > currentScroll) {
-
-          clearInterval(int)
-        } else {
-          currentScroll = currentScroll - 12
-          document.documentElement.scrollTop = currentScroll
-        }
-      }
-    }
   },
   created() {
 
