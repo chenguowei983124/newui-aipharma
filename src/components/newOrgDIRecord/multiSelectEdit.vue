2c7ea5c0bc1b263621479e88b4135c1ee4eef1ff
鮑勝 <shou.hou@hh-c.co.jp>
2021-10-15 10:00:47 +0900

    fix conflict

---
+++ src/components/newOrgDIRecord/multiSelectEdit.vue
@@ -0,0 +1,30 @@
+<template>
+  <Multiselect
+      class="h-7.5"
+      mode="tags"
+      placeholder=""
+      :filterResults="false"
+      :minChars="1"
+      :resolveOnLoad="false"
+      :delay="0"
+      :searchable="true"
+      :options="multiSelectItemList"
+      :classes="$constant.multiselectCss"
+  ></Multiselect>
+</template>
+
+<script>
+import Multiselect from '@vueform/multiselect'
+
+export default {
+  name: 'multiSelectEdit',
+  props: {
+    multiSelectItemList:{}
+  },
+  components: {
+    Multiselect,
+  },
+}
+</script>
+<style>
+</style>
--- src/components/newOrgDIRecord/newOrgDIRecordMain.vue
+++ src/components/newOrgDIRecord/newOrgDIRecordMain.vue
@@ -199,7 +199,7 @@
                             />
                         </div>
                         <div id="pmid" class="mt-3">
-                            <a :href="url.pmid" target="_blank" class="flex">
+                            <a :href="url.pmid.lable" target="_blank" class="flex">
                                 <label
                                     class="notoSansJpAndFourteenBold underline"
                                 >
@@ -209,30 +209,38 @@
                                     class="hover:opacity-50 active:opacity-50"
                                 ></external-link>
                             </a>
-                            <input
-                                v-model="base.pmid"
-                                class="
-                                    block
-                                    h-10
-                                    w-full
-                                    NotoSansJp-normal
-                                    rounded-sm
-                                    pl-4
-                                    placeholder-gray-500
-                                    focus:placeholder-opacity-0
-                                    ring-1
-                                    border-transparent
-                                    focus:outline-none
-                                    focus:ring-1
-                                    focus:ring-326EB5Lins
-                                    focus:border-transparent
-                                "
-                                type="text"
-                                placeholder="PMID"
-                            />
+                            <div class="flex-wrap md:flex">
+                                <input
+                                    v-model="base.pmid"
+                                    class="
+                                        block
+                                        h-10
+                                        w-full md:w-1/2
+                                        NotoSansJp-normal
+                                        rounded-sm
+                                        pl-4
+                                        placeholder-gray-500
+                                        focus:placeholder-opacity-0
+                                        ring-1
+                                        border-transparent
+                                        focus:outline-none
+                                        focus:ring-1
+                                        focus:ring-326EB5Lins
+                                        focus:border-transparent
+                                    "
+                                    type="text"
+                                    placeholder="PMID"
+                                    :onInput="onInputPmid"
+                                />
+                                <a :href="url.pmid.text" target="_blank" class="flex">
+                                  <label class="notoSansJpAndFourteenBold underline cursor-pointer">
+                                    {{url.pmid.text}}
+                                  </label>
+                                </a>
+                            </div>
                         </div>
                         <div id="doi" class="mt-3">
-                            <a :href="url.doi" target="_blank" class="flex">
+                            <a :href="url.doi.lable" target="_blank" class="flex">
                                 <label
                                     class="notoSansJpAndFourteenBold underline"
                                 >
@@ -242,27 +250,35 @@
                                     class="hover:opacity-50 active:opacity-50"
                                 ></external-link>
                             </a>
-                            <input
-                                v-model="base.doi"
-                                class="
-                                    block
-                                    h-10
-                                    w-full
-                                    NotoSansJp-normal
-                                    rounded-sm
-                                    pl-4
-                                    placeholder-gray-500
-                                    focus:placeholder-opacity-0
-                                    ring-1
-                                    border-transparent
-                                    focus:outline-none
-                                    focus:ring-1
-                                    focus:ring-326EB5Lins
-                                    focus:border-transparent
-                                "
-                                type="text"
-                                placeholder="DOI"
-                            />
+                            <div class="flex-wrap md:flex">
+                                <input
+                                    v-model="base.doi"
+                                    class="
+                                        block
+                                        h-10
+                                        w-full md:w-1/2
+                                        NotoSansJp-normal
+                                        rounded-sm
+                                        pl-4
+                                        placeholder-gray-500
+                                        focus:placeholder-opacity-0
+                                        ring-1
+                                        border-transparent
+                                        focus:outline-none
+                                        focus:ring-1
+                                        focus:ring-326EB5Lins
+                                        focus:border-transparent
+                                    "
+                                    type="text"
+                                    placeholder="DOI"
+                                    :onInput="onInputDoi"
+                                />
+                              <a :href="url.doi.text" target="_blank" class="flex">
+                                <label class="notoSansJpAndFourteenBold underline cursor-pointer">
+                                  {{url.doi.text}}
+                                </label>
+                              </a>
+                            </div>
                         </div>
                         <div id="file" class="mt-3">
                             <label class="notoSansJpAndFourteenBold">
@@ -366,97 +382,10 @@
                                 薬の分類
                             </label>
                             <div id="msMediTypes">
-                                <Multiselect
-                                    class="h-7.5"
-                                    v-model="detail.mediTypes"
-                                    mode="tags"
-                                    placeholder=""
-                                    :filterResults="false"
-                                    :minChars="1"
-                                    :resolveOnLoad="false"
-                                    :delay="0"
-                                    :searchable="true"
-                                    :options="multiSelectItemList"
-                                    :classes="{
-                                        container:
-                                            'relative mx-auto w-full flex items-center justify-end box-border cursor-pointer border border-gray-300 rounded bg-white text-base leading-snug outline-none',
-                                        containerDisabled:
-                                            'cursor-default bg-gray-100',
-                                        containerOpen: 'rounded-b-none',
-                                        containerOpenTop: 'rounded-t-none',
-                                        containerActive:
-                                            'ring ring-green-500 ring-opacity-30',
-                                        singleLabel:
-                                            'flex items-center h-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5',
-                                        multipleLabel:
-                                            'flex items-center h-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5',
-                                        search: 'w-full absolute inset-0 outline-none appearance-none box-border border-0 text-base font-sans bg-white rounded pl-3.5',
-                                        tags: 'flex-grow flex-shrink flex flex-wrap items-center mt-1 pl-2',
-                                        tag: 'bg-gray-300 text-black notoSansJpAndTwelveRegular pl-2 rounded-full border-2 mr-1 mb-1 flex items-center whitespace-nowrap',
-                                        tagDisabled: 'pr-2 opacity-50',
-                                        tagRemove:
-                                            'flex items-center justify-center p-1 mx-0.5 rounded-sm hover:bg-black hover:bg-opacity-10 group',
-                                        tagRemoveIcon:
-                                            'bg-multiselect-remove bg-center bg-no-repeat opacity-30 inline-block w-3 h-3 group-hover:opacity-60',
-                                        tagsSearchWrapper:
-                                            'inline-block relative mx-1 mb-1 flex-grow flex-shrink h-full',
-                                        tagsSearch:
-                                            'absolute inset-0 border-0 outline-none appearance-none p-0 text-base font-sans box-border w-full',
-                                        tagsSearchCopy:
-                                            'invisible whitespace-pre-wrap inline-block h-px',
-                                        placeholder:
-                                            'flex items-center h-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5 text-gray-400',
-                                        caret: 'bg-multiselect-caret bg-center bg-no-repeat w-2.5 h-4 py-px box-content mr-3.5 relative z-10 opacity-40 flex-shrink-0 flex-grow-0 transition-transform transform pointer-events-none',
-                                        caretOpen:
-                                            'rotate-180 pointer-events-auto',
-                                        clear: 'pr-3.5 relative z-10 opacity-40 transition duration-300 flex-shrink-0 flex-grow-0 flex hover:opacity-80',
-                                        clearIcon:
-                                            'bg-multiselect-remove bg-center bg-no-repeat w-2.5 h-4 py-px box-content inline-block',
-                                        spinner:
-                                            'bg-multiselect-spinner bg-center bg-no-repeat w-4 h-4 z-10 mr-3.5 animate-spin flex-shrink-0 flex-grow-0',
-                                        dropdown:
-                                            'max-h-60 absolute -left-px -right-px bottom-0 transform translate-y-full border border-gray-300 -mt-px overflow-y-scroll z-50 bg-white flex flex-col rounded-b',
-                                        dropdownTop:
-                                            '-translate-y-full top-px bottom-auto flex-col-reverse rounded-b-none rounded-t',
-                                        dropdownHidden: 'hidden',
-                                        options:
-                                            'flex flex-col p-0 m-0 list-none',
-                                        optionsTop: 'flex-col-reverse',
-                                        group: 'p-0 m-0',
-                                        groupLabel:
-                                            'flex text-sm box-border items-center justify-start text-left py-1 px-3 font-semibold bg-gray-200 cursor-default leading-normal',
-                                        groupLabelPointable: 'cursor-pointer',
-                                        groupLabelPointed:
-                                            'bg-gray-300 text-gray-700',
-                                        groupLabelSelected:
-                                            'bg-green-600 text-white',
-                                        groupLabelDisabled:
-                                            'bg-gray-100 text-gray-300 cursor-not-allowed',
-                                        groupLabelSelectedPointed:
-                                            'bg-green-600 text-white opacity-90',
-                                        groupLabelSelectedDisabled:
-                                            'text-green-100 bg-green-600 bg-opacity-50 cursor-not-allowed',
-                                        groupOptions: 'p-0 m-0',
-                                        option: 'flex items-center justify-start box-border text-left cursor-pointer text-base leading-snug py-2 px-3',
-                                        optionPointed:
-                                            'text-gray-800 bg-gray-100',
-                                        optionSelected:
-                                            'text-white bg-green-500',
-                                        optionDisabled:
-                                            'text-gray-300 cursor-not-allowed',
-                                        optionSelectedPointed:
-                                            'text-white bg-green-500 opacity-90',
-                                        optionSelectedDisabled:
-                                            'text-green-100 bg-green-500 bg-opacity-50 cursor-not-allowed',
-                                        noOptions:
-                                            'py-2 px-3 text-gray-600 bg-white',
-                                        noResults:
-                                            'py-2 px-3 text-gray-600 bg-white',
-                                        fakeInput:
-                                            'bg-transparent absolute left-0 right-0 -bottom-px w-full h-px border-0 p-0 appearance-none outline-none text-transparent',
-                                        spacer: 'h-9 py-px box-content',
-                                    }"
-                                ></Multiselect>
+                                <multiselectEdit
+                                  v-model="detail.mediTypes"
+                                  :multiSelectItemList="multiSelectItemList"
+                                ></multiselectEdit>
                             </div>
                         </div>
                         <div id="quesClass" class="mt-3">
@@ -692,19 +621,10 @@
                                 マルチセレクト項目
                             </label>
                             <div id="msMultiSelectItem">
-                                <Multiselect
-                                    class="h-7.5"
-                                    v-model="detail.multiSelectItem"
-                                    mode="tags"
-                                    placeholder=""
-                                    :filterResults="false"
-                                    :minChars="1"
-                                    :resolveOnLoad="false"
-                                    :delay="0"
-                                    :searchable="true"
-                                    :options="multiSelectItemList"
-                                    :classes="$constant.multiselectCss"
-                                ></Multiselect>
+                                <multiselectEdit
+                                  v-model="detail.multiSelectItem"
+                                  :multiSelectItemList="multiSelectItemList"
+                                ></multiselectEdit>
                             </div>
                         </div>
                         <div id="textItem" class="mt-3">
@@ -825,9 +745,11 @@
 import newOrgDIRecordButtons from './newOrgDIRecordButtons.vue'
 import vueSingleSelect from '../common/dropdown/vueSingleSelect.vue'
 import litepieDatepicker from '../common/dateRange/litepie-datepicker.vue'
-import tinymceEdit from './tinymceEdit.vue'
-import Multiselect from '@vueform/multiselect'
 import externalLink from '../common/svgImage/extarnalLink.vue'
+import tinymceEdit from './tinymceEdit.vue'
+import multiselectEdit from './multiSelectEdit.vue'
+const BASE_URL_PMID = "https://www.ncbi.nlm.nih.gov/pubmed/"
+const BASE_URL_DOI = "http://www.google.com/"
 
 export default {
     components: {
@@ -835,14 +757,20 @@ export default {
         vueSingleSelect,
         litepieDatepicker,
         tinymceEdit,
-        Multiselect,
+        multiselectEdit,
         externalLink,
     },
     data() {
         return {
             url: {
-                pmid: 'https://www.pmda.go.jp/',
-                doi: 'https://www.pmda.go.jp/',
+            pmid: {
+              lable: 'https://www.pmda.go.jp/',
+              text: '',
+            },
+            doi: {
+              lable: 'http://www.google.com/',
+              text: '',
+            },
             },
             prefessionList: [
                 { value: '0', title: '薬剤師' },
@@ -903,6 +831,22 @@ export default {
         }
     },
     methods: {
+        onInputPmid (e) {
+          // console.log('onChangeURL',e)
+          if (!!e.target.value) {
+            this.url.pmid.text = BASE_URL_PMID + e.target.value
+          } else {
+            this.url.pmid.text = ''
+          }
+        },
+        onInputDoi (e) {
+          // console.log('onChangeURL',e)
+          if (!!e.target.value) {
+            this.url.doi.text = BASE_URL_DOI + e.target.value
+          } else {
+            this.url.doi.text = ''
+          }
+        },
         onFileChange(e) {
             console.log('onFileChange-1', e)
             const idx = e.target.id.slice(e.target.id.lastIndexOf('_') + 1)
