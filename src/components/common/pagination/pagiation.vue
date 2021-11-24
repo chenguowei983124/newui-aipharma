3b285d0055c69b8d19f8695dda877059ff467b15
*VYHoN0tB0U <guowei.chen@hh-c.co.jp>
2021-11-24 10:17:44 +0900

    ページネーション5件対応

--- src/components/common/pagination/pagiation.vue
+++ src/components/common/pagination/pagiation.vue
@@ -28,15 +28,17 @@
                 v-html="prevText"
             ></a>
         </li>
-
-        <li
-            v-for="page in pages"
-            :key="page"
-            @click="
+        <!-- {{
+            pages
+        }} -->
+        <!-- @click="
                 page.selected
-                    ? prevFivePage()
+                    ? prevFivePage(pages)
                     : handlePageSelected(page.index + 1)
-            "
+            " -->
+        <li
+            v-for="(page, key, index) in pages"
+            :key="index"
             :class="[
                 pageClass,
                 page.selected ? activeClass : '',
@@ -44,10 +46,12 @@
                 page.breakView ? breakViewClass : '',
             ]"
         >
+            <!-- {{ index }} -->
             <a
                 v-if="page.breakView"
                 :class="[pageLinkClass, breakViewLinkClass]"
                 tabindex="0"
+                @click="index < 3 ? prevFivePage(page) : nextFivePage(page)"
                 ><slot name="breakViewContent">{{ breakViewText }}</slot></a
             >
             <a v-else-if="page.disabled" :class="pageLinkClass" tabindex="0">{{
@@ -164,246 +168,250 @@
 
 <script>
 export default {
-  props: {
-    value: {
-      type: Number,
+    props: {
+        value: {
+            type: Number,
+        },
+        pageCount: {
+            type: Number,
+            required: true,
+        },
+        forcePage: {
+            type: Number,
+        },
+        clickHandler: {
+            type: Function,
+            default: () => {},
+        },
+        pageRange: {
+            type: Number,
+            default: 3,
+        },
+        marginPages: {
+            type: Number,
+            default: 1,
+        },
+        prevText: {
+            type: String,
+            default: 'Prev',
+        },
+        nextText: {
+            type: String,
+            default: 'Next',
+        },
+        breakViewText: {
+            type: String,
+            default: '…',
+        },
+        // breakViewText: {
+        //   type: String,
+        //   default: '…'
+        // },
+        containerClass: {
+            type: String,
+        },
+        pageClass: {
+            type: String,
+        },
+        pageLinkClass: {
+            type: String,
+        },
+        prevClass: {
+            type: String,
+        },
+        prevLinkClass: {
+            type: String,
+        },
+        nextClass: {
+            type: String,
+        },
+        nextLinkClass: {
+            type: String,
+        },
+        breakViewClass: {
+            type: String,
+        },
+        breakViewLinkClass: {
+            type: String,
+        },
+        activeClass: {
+            type: String,
+            default: 'active',
+        },
+        disabledClass: {
+            type: String,
+            default: 'disabled',
+        },
+        noLiSurround: {
+            type: Boolean,
+            default: false,
+        },
+        firstLastButton: {
+            type: Boolean,
+            default: false,
+        },
+        firstButtonText: {
+            type: String,
+            default: 'First',
+        },
+        lastButtonText: {
+            type: String,
+            default: 'Last',
+        },
+        hidePrevNext: {
+            type: Boolean,
+            default: false,
+        },
     },
-    pageCount: {
-      type: Number,
-      required: true,
-    },
-    forcePage: {
-      type: Number,
-    },
-    clickHandler: {
-      type: Function,
-      default: () => { },
-    },
-    pageRange: {
-      type: Number,
-      default: 3,
-    },
-    marginPages: {
-      type: Number,
-      default: 1,
-    },
-    prevText: {
-      type: String,
-      default: 'Prev',
-    },
-    nextText: {
-      type: String,
-      default: 'Next',
-    },
-    breakViewText: {
-      type: String,
-      default: '…',
-    },
-    // breakViewText: {
-    //   type: String,
-    //   default: '…'
-    // },
-    containerClass: {
-      type: String,
-    },
-    pageClass: {
-      type: String,
-    },
-    pageLinkClass: {
-      type: String,
-    },
-    prevClass: {
-      type: String,
-    },
-    prevLinkClass: {
-      type: String,
-    },
-    nextClass: {
-      type: String,
-    },
-    nextLinkClass: {
-      type: String,
-    },
-    breakViewClass: {
-      type: String,
-    },
-    breakViewLinkClass: {
-      type: String,
-    },
-    activeClass: {
-      type: String,
-      default: 'active',
-    },
-    disabledClass: {
-      type: String,
-      default: 'disabled',
-    },
-    noLiSurround: {
-      type: Boolean,
-      default: false,
-    },
-    firstLastButton: {
-      type: Boolean,
-      default: false,
-    },
-    firstButtonText: {
-      type: String,
-      default: 'First',
-    },
-    lastButtonText: {
-      type: String,
-      default: 'Last',
-    },
-    hidePrevNext: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  beforeUpdate() {
-    if (this.forcePage === undefined) return
-    if (this.forcePage !== this.selected) {
-      this.selected = this.forcePage
-    }
-  },
-  computed: {
-    selected: {
-      get: function () {
-        return this.value || this.innerValue
-      },
-      set: function (newValue) {
-        this.innerValue = newValue
-      },
-    },
-    pages: function () {
-      let items = {}
-      if (this.pageCount <= this.pageRange) {
-        for (let index = 0; index < this.pageCount; index++) {
-          let page = {
-            index: index,
-            content: index + 1,
-            selected: index === this.selected - 1,
-          }
-          items[index] = page
+    beforeUpdate() {
+        if (this.forcePage === undefined) return
+        if (this.forcePage !== this.selected) {
+            this.selected = this.forcePage
         }
-      } else {
-        const halfPageRange = Math.floor(this.pageRange / 2)
+    },
+    computed: {
+        selected: {
+            get: function () {
+                return this.value || this.innerValue
+            },
+            set: function (newValue) {
+                this.innerValue = newValue
+            },
+        },
+        pages: function () {
+            let items = {}
+            if (this.pageCount <= this.pageRange) {
+                for (let index = 0; index < this.pageCount; index++) {
+                    let page = {
+                        index: index,
+                        content: index + 1,
+                        selected: index === this.selected - 1,
+                    }
+                    items[index] = page
+                }
+            } else {
+                const halfPageRange = Math.floor(this.pageRange / 2)
 
-        let setPageItem = (index) => {
-          let page = {
-            index: index,
-            content: index + 1,
-            selected: index === this.selected - 1,
-          }
+                let setPageItem = (index) => {
+                    let page = {
+                        index: index,
+                        content: index + 1,
+                        selected: index === this.selected - 1,
+                    }
 
-          items[index] = page
+                    items[index] = page
+                }
+
+                let setBreakView = (index) => {
+                    let breakView = {
+                        disabled: true,
+                        breakView: true,
+                    }
+
+                    items[index] = breakView
+                }
+
+                // 1st - loop thru low end of margin pages
+                for (let i = 0; i < this.marginPages; i++) {
+                    setPageItem(i)
+                }
+
+                // 2nd - loop thru selected range
+                let selectedRangeLow = 0
+                if (this.selected - halfPageRange > 0) {
+                    selectedRangeLow = this.selected - 1 - halfPageRange
+                }
+
+                let selectedRangeHigh = selectedRangeLow + this.pageRange - 1
+                if (selectedRangeHigh >= this.pageCount) {
+                    selectedRangeHigh = this.pageCount - 1
+                    selectedRangeLow = selectedRangeHigh - this.pageRange + 1
+                }
+
+                for (
+                    let i = selectedRangeLow;
+                    i <= selectedRangeHigh && i <= this.pageCount - 1;
+                    i++
+                ) {
+                    setPageItem(i)
+                }
+
+                // Check if there is breakView in the left of selected range
+                if (selectedRangeLow > this.marginPages) {
+                    setBreakView(selectedRangeLow - 1)
+                }
+
+                // Check if there is breakView in the right of selected range
+                if (selectedRangeHigh + 1 < this.pageCount - this.marginPages) {
+                    setBreakView(selectedRangeHigh + 1)
+                }
+
+                // 3rd - loop thru high end of margin pages
+                for (
+                    let i = this.pageCount - 1;
+                    i >= this.pageCount - this.marginPages;
+                    i--
+                ) {
+                    setPageItem(i)
+                }
+            }
+            return items
+        },
+    },
+    data() {
+        return {
+            innerValue: 1,
         }
-
-        let setBreakView = (index) => {
-          let breakView = {
-            disabled: true,
-            breakView: true,
-          }
-
-          items[index] = breakView
-        }
-
-        // 1st - loop thru low end of margin pages
-        for (let i = 0; i < this.marginPages; i++) {
-          setPageItem(i)
-        }
-
-        // 2nd - loop thru selected range
-        let selectedRangeLow = 0
-        if (this.selected - halfPageRange > 0) {
-          selectedRangeLow = this.selected - 1 - halfPageRange
-        }
-
-        let selectedRangeHigh = selectedRangeLow + this.pageRange - 1
-        if (selectedRangeHigh >= this.pageCount) {
-          selectedRangeHigh = this.pageCount - 1
-          selectedRangeLow = selectedRangeHigh - this.pageRange + 1
-        }
-
-        for (
-          let i = selectedRangeLow;
-          i <= selectedRangeHigh && i <= this.pageCount - 1;
-          i++
-        ) {
-          setPageItem(i)
-        }
-
-        // Check if there is breakView in the left of selected range
-        if (selectedRangeLow > this.marginPages) {
-          setBreakView(selectedRangeLow - 1)
-        }
-
-        // Check if there is breakView in the right of selected range
-        if (selectedRangeHigh + 1 < this.pageCount - this.marginPages) {
-          setBreakView(selectedRangeHigh + 1)
-        }
-
-        // 3rd - loop thru high end of margin pages
-        for (
-          let i = this.pageCount - 1;
-          i >= this.pageCount - this.marginPages;
-          i--
-        ) {
-          setPageItem(i)
-        }
-      }
-      return items
     },
-  },
-  data() {
-    return {
-      innerValue: 1,
-    }
-  },
-  methods: {
-    handlePageSelected(selected) {
-      if (this.selected === selected) return
+    methods: {
+        handlePageSelected(selected) {
+            if (this.selected === selected) return
 
-      this.innerValue = selected
-      // console.log('pageinput', selected)
-      this.$emit('input', selected)
-      this.clickHandler(selected)
-    },
-    prevPage() {
-      if (this.selected <= 1) return
-      this.handlePageSelected(this.selected - 1)
-    },
-    nextPage() {
-      if (this.selected >= this.pageCount) return
-      this.handlePageSelected(this.selected + 1)
-    },
+            this.innerValue = selected
+            this.$emit('input', selected)
+            this.clickHandler(selected)
+        },
+        prevPage() {
+            if (this.selected <= 1) return
+            this.handlePageSelected(this.selected - 1)
+        },
+        nextPage() {
+            if (this.selected >= this.pageCount) return
+            this.handlePageSelected(this.selected + 1)
+        },
 
-    prevFivePage() {
-      if (this.selected <= 5) return
-      this.handlePageSelected(this.selected - 5)
-    },
-    nextFivePage() {
-      if (this.selected >= this.pageCount) return
+        prevFivePage(page) {
+            if (this.selected <= 5) {
+                this.handlePageSelected(1)
+            } else {
+                this.handlePageSelected(this.selected - 5)
+            }
+        },
+        nextFivePage(page) {
+            if (this.selected + 5 >= this.pageCount) {
+                this.handlePageSelected(this.pageCount)
+            } else {
+                this.handlePageSelected(this.selected + 5)
+            }
+        },
 
-      this.handlePageSelected(this.selected + 5)
-    },
+        firstPageSelected() {
+            return this.selected === 1
+        },
+        lastPageSelected() {
+            return this.selected === this.pageCount || this.pageCount === 0
+        },
+        selectFirstPage() {
+            if (this.selected <= 1) return
 
-    firstPageSelected() {
-      return this.selected === 1
-    },
-    lastPageSelected() {
-      return this.selected === this.pageCount || this.pageCount === 0
-    },
-    selectFirstPage() {
-      if (this.selected <= 1) return
+            this.handlePageSelected(1)
+        },
+        selectLastPage() {
+            if (this.selected >= this.pageCount) return
 
-      this.handlePageSelected(1)
+            this.handlePageSelected(this.pageCount)
+        },
     },
-    selectLastPage() {
-      if (this.selected >= this.pageCount) return
-
-      this.handlePageSelected(this.pageCount)
-    },
-  },
 }
 </script>
 
